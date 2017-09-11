c5155aede7fccfce927df88747868418b2d6de9d
Tóthné Fehér Edit Mária <feher.edit.maria@gmail.com>
2017-09-11 18:00:40 +0200

    add DOMContentLoaded event

--- todoapp.js
+++ todoapp.js
@@ -1,40 +1,40 @@
-(function () {
-
+document.addEventListener("DOMContentLoaded", function(){
+  
   ///// Create and set HTML Elements \\\\\
-
+  
   var pageTitle = document.createElement("h1");
   pageTitle.id = "page-title";
-  pageTitle.innerHTML = "My Things";
-
+  pageTitle.innerHTML = "My Things To Do";
+  
   var toDoWrapper = document.createElement("div");
   toDoWrapper.id = "todo-wrapper";
-
+  
   var toDoForm = document.createElement("form");
   toDoForm.id = "todo-form";
-
+  
   var toDoFormText = document.createElement("input");
   toDoFormText.id = "todo-form-text";
   toDoFormText.setAttribute("type", "text");
   toDoFormText.setAttribute("placeholder", "Things To Do");
   toDoFormText.setAttribute("autofocus", "true");
-
+  
   var toDoFormButton = document.createElement("button");
   toDoFormButton.id = "todo-form-submit";
   toDoFormButton.setAttribute("type", "button");
   toDoFormButton.innerHTML = "To The List";
-
+  
   var toDoList = document.createElement("ul");
   toDoList.id = "todo-list";
-
+  
   var newListButton = document.createElement("button");
   newListButton.id = "new-list-button";
   newListButton.setAttribute("type", "button");
   newListButton.innerHTML = "New List";
   newListButton.style.display = "none";
-
-
+  
+  
   ///// Insert the created HTML Elements \\\\\
-
+  
   document.body.appendChild(pageTitle);
   document.body.appendChild(toDoWrapper);
   toDoWrapper.appendChild(toDoForm);
@@ -42,12 +42,12 @@
   toDoForm.appendChild(toDoFormButton);
   toDoWrapper.appendChild(toDoList);
   toDoWrapper.appendChild(newListButton);
-
-
-  ///// Get the user's things to do \\\\\
+  
+  
+  ///// Get the things \\\\\
 
   function getThings(t) {
-    if (t !== "") {
+    if(t !== "") {
       var toDoListItem = document.createElement("li");
       var text = document.createTextNode(t);
       toDoListItem.appendChild(text);
@@ -57,30 +57,32 @@
       newListButton.style.display = "inline-block";
     }
   }
-
-  toDoFormButton.addEventListener("click", function () {
+  
+  toDoFormButton.addEventListener("click", function() {
     getThings(toDoFormText.value);
   });
-
-  toDoFormText.addEventListener("keypress", function (event) {
-    if (event.keyCode == 13) {
+  
+  toDoFormText.addEventListener("keypress", function(event) {
+    if(event.keyCode == 13) {
       event.preventDefault();
       getThings(toDoFormText.value);
     }
-  })
-
-
+  });
+  
+  
   ///// Style list items \\\\\
-
-  toDoList.addEventListener("click", function (event) {
-    if (event.target && event.target.nodeName === "LI") {
+  
+  toDoList.addEventListener("click", function(event) {
+    if(event.target && event.target.nodeName === "LI") {
       var item = event.target;
-      if (item.style.textDecoration === "none") {
-        item.style.textDecoration = "line-through";
-      } else {
-        item.style.textDecoration = "none";
-      }
+        if (item.style.textDecoration === "none") {
+          item.style.textDecoration = "line-through";
+        } else {
+          item.style.textDecoration = "none";
+        }
     }
-  })
+  });
+  
+  
+});
 
-})();
